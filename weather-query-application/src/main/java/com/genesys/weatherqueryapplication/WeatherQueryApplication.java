50e1f046bf723b282fe101065eda4b80586632c7
Tobillion_19 <Melesheku@hotmail.com>
2023-07-13 23:07:00 +0100

    Initial push of Weather Query Application

---
+++ weather-query-application/src/main/java/com/genesys/weatherqueryapplication/WeatherQueryApplication.java
@@ -0,0 +1,13 @@
+package com.genesys.weatherqueryapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherQueryApplication.class, args);
+	}
+
+}
---
+++ weather-query-application/src/test/java/com/genesys/weatherqueryapplication/WeatherQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesys.weatherqueryapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
